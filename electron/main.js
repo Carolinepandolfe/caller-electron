5138b0b571fb268b931fcf00e4d3d3662562f6aa
Carolinepandolfe <carolinepandolfe@yahoo.com.br>
2020-10-22 22:06:25 -0300

    Add icon on dock

--- electron/main.js
+++ electron/main.js
@@ -1,27 +1,31 @@
 const { app, BrowserWindow, nativeImage } = require('electron');
-const isDev = require('electron-is-dev');   
+const isDev = require('electron-is-dev');
 const path = require('path');
- 
- 
-function createWindow() {
-    const icon = nativeImage.createFromPath(`${app.getAppPath()}/sp_mais.png`);
 
-    if (app.dock) {
-        app.dock.setIcon(icon);
-    }
-    
-    mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        show: false
-    });
-    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
- 
-    mainWindow.loadURL(startURL);
- 
-    mainWindow.once('ready-to-show', () => mainWindow.show());
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+function createWindow() {
+  const icon = nativeImage.createFromPath(
+    `${app.getAppPath()}/electron/icon .png`,
+  );
+
+  if (app.dock) {
+    app.dock.setIcon(icon);
+  }
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    icon: icon,
+  });
+  const startURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once('ready-to-show', () => mainWindow.show());
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 app.on('ready', createWindow);
